5f8fd4ec9bbd6c2b9630f69ca2f688983fcc1610
Anshul Raman <anshul.raman@live.com>
2022-07-29 00:33:52 +0530

    changed to tailwind and removed chakraui

---
+++ pages/blogs.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const BlogPage: NextPage = () => {
+  return (
+    <main className="flex flex-col justify-center content-center min-h-screen p-3 ">
+      {/* Man center div */}
+      <div className="  max-w-2xl mx-auto ">
+        <h1 className="font-mono ">Under Construction :)</h1>
+      </div>
+    </main>
+  );
+};
+
+export default BlogPage;
